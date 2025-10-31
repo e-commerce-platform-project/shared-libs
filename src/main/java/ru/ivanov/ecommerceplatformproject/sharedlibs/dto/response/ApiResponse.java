773dadeb6d9bd6ca74d2d6eb26188cd60d43db24
Anton Ivanov <ivanova21600@mail.ru>
2025-11-01 01:19:09 +0300

    refactoring

--- src/main/java/ru/ivanov/ecommerceplatformproject/sharedlibs/dto/response/ApiResponse.java
+++ src/main/java/ru/ivanov/ecommerceplatformproject/sharedlibs/dto/response/ApiResponse.java
@@ -3,22 +3,17 @@ package ru.ivanov.ecommerceplatformproject.sharedlibs.dto.response;
 import java.time.LocalDateTime;
 
 
-public class ApiResponse {
-    private final String message;
-    private final boolean success;
-    private final LocalDateTime timestamp;
+public record ApiResponse(
+        String message,
+        boolean success,
+        LocalDateTime timestamp
 
-    public ApiResponse(String message, boolean success, LocalDateTime timestamp) {
-        this.message = message;
-        this.success = success;
-        this.timestamp = timestamp;
-    }
-
+) {
     public static ApiResponse success(String message) {
-        return new ApiResponse(message, true, LocalDateTime.now());
+        return new ApiResponse(message, true, java.time.LocalDateTime.now());
     }
 
     public static ApiResponse error(String message) {
-        return new ApiResponse(message, false, LocalDateTime.now());
+        return new ApiResponse(message, false, java.time.LocalDateTime.now());
     }
 }
